13ce2fedbd500fd5ccb674ed5fd02c91b4aca394
IDEALE02 <ideale02@Admins-puu.local>
2018-07-03 11:12:10 +0700

    backend hospital

--- application/controllers/backend/Hospital.php
+++ application/controllers/backend/Hospital.php
@@ -4,11 +4,107 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Hospital extends MY_Controller {
 	public function __construct(){
 		parent::__construct();
+		$this->load->model('backend/main_model');
+		$this->load->model('backend/hospital_model');
 	}
 	public function index()
 	{
 		$data['page'] = 'hospital';
 		$data['link_img'] = base_url('assets/frontend/images/');
+		$data['link_cover'] = base_url('upload/thumb/cover/');
+		$data['cover'] = $this->main_model->selectCover('hospital');
 		$this->twig->display('@b/hospital', $data);
+	}
+	public function uploadFile($folder, $name){
+		if(isset($_FILES[$name])){
+			$upload_state = $this->imageManipulation($folder, $name);
+			if($upload_state['status'] == 'success'){
+				return $upload_state['files_name'];
+			}
+			else{
+				echo json_encode(array('msg'=>$upload_state['msg'], 'status'=>'error'));
+				exit();
+			}
+		}else{
+			return null;
+		}
+	}
+	public function setCover()
+	{
+		if($this->input->post()){
+			$data['status'] = 0;
+			$data['msg'] = 'ไม่สามารถบันทึกข้อมูลได้';
+			$_POST['img'] = $this->uploadFile('cover', 'img');
+			$res = $this->main_model->updateCover($_POST);
+			if($res > 0) {
+				$data['status'] = 1;
+				$data['msg'] = 'บันทึกข้อมูลสำเร็จ';	
+			}
+			unset($_POST);
+			echo json_encode($data);
+			exit();
+		}
+	}
+
+	// hospital
+	public function tableHospital(){
+		$data['hospital'] = $this->hospital_model->selectHospital();
+		$this->twig->display('@b/hospital-table', $data);
+	}
+	public function addHospital(){
+		if($this->input->post()){
+			$data = array();
+			$data['status'] = 0;
+			$data['msg'] = 'ไม่สามารถบันทึกข้อมูลได้';
+			$_POST['icon'] = $this->uploadFile('hospital', 'icon');
+			$res = $this->hospital_model->insertHospital($_POST);
+			if($res > 0) {
+				$data['status'] = 1;
+				$data['msg'] = 'บันทึกข้อมูลสำเร็จ';
+			}
+			unset($_POST);
+			echo json_encode($data);
+			exit();
+		}
+		$data['mode'] = 'add';
+		$this->twig->display('@b/hospital-form', $data);
+	}
+	public function editHospital($id = 0){
+		if($this->input->post()){
+			$data = array();
+			$data['status'] = 0;
+			$data['msg'] = 'ไม่สามารถบันทึกข้อมูลได้';
+			$_POST['icon'] = $this->uploadFile('hospital', 'icon');
+			$res = $this->hospital_model->updateHospital($_POST);
+			if($res > 0) {
+				$data['status'] = 1;
+				$data['msg'] = 'บันทึกข้อมูลสำเร็จ';	
+			}
+			unset($_POST);
+			echo json_encode($data);
+			exit();
+		}
+		$data['mode'] = 'edit';
+		$data['data'] = $this->hospital_model->selectHospitalByID($id);
+		$data['id'] = $id;
+		$this->twig->display('@b/hospital-form', $data);
+	}
+	public function deleteHospital($id)
+	{
+		$res = $this->hospital_model->deleteHospital($id);
+		echo $res;
+	}
+	public function setHospital()
+	{
+		if($this->input->post()){
+			$data = array();
+			$res = $this->hospital_model->updateHospitalActions($_POST);
+			if($res > 0) {
+				$data['status'] = 1;
+				$data['msg'] = 'บันทึกข้อมูลสำเร็จ';	
+			}
+			echo json_encode($data);
+			exit();
+		}
 	}
 }
---
+++ application/models/backend/Hospital_model.php
@@ -0,0 +1,79 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Hospital_model extends CI_Model{
+	function __construct(){
+		parent::__construct();
+	}
+	// hospital
+	public function selectHospital()
+	{
+		$query = $this->db->select('*')
+				->from('tb_hospital')
+				->where('status != -1')
+				->order_by('seq', 'ASC')
+				->order_by('id', 'DESC')
+				->get();
+		$data = $query->result_array();
+		return $data;	
+	}
+	public function selectHospitalByID($id)
+	{
+		$query = $this->db->select('*')
+				->from('tb_hospital')
+				->where('id', $id)
+				->where('status != -1')
+				->get();
+		$data = $query->result_array();
+		return sizeof($data) > 0 ? $data[0] : null;	
+	}
+	public function insertHospital($value){
+		$values = array(
+			'name_th' => $value['name_th'],
+			'name_en' => $value['name_en'],
+			'room_th' => json_encode($value['room_th']),
+			'room_en' => json_encode($value['room_en']),
+			'price' => json_encode($value['price']),
+			'tel' => $value['tel'],
+			'link' => $value['link'],
+			'status' => 0,
+			'created_date' => date("Y-m-d H:i:s"),
+		);
+
+		$this->db->insert('tb_hospital',$values);
+		return $this->db->insert_id();
+	}
+	public function updateHospital($value){
+		$values = array(
+			'name_th' => $value['name_th'],
+			'name_en' => $value['name_en'],
+			'room_th' => json_encode($value['room_th']),
+			'room_en' => json_encode($value['room_en']),
+			'price' => json_encode($value['price']),
+			'tel' => $value['tel'],
+			'link' => $value['link'],
+			'updated_date' => date("Y-m-d H:i:s"),
+		);
+
+		$this->db->where('id', $value['id']);
+		$this->db->update('tb_hospital', $values);
+		return $this->db->affected_rows();
+	}
+	public function updateHospitalActions($data){
+		foreach($data['id'] as $key => $id){
+			$values = array(
+				'seq' => $data['seq'][$key],
+				'status' => empty($data['status'][$key]) ? 0 : 1
+			);
+			$this->db->where('id', $id);
+			$this->db->update('tb_hospital', $values);
+		}
+		return 1;
+	}
+	public function deleteHospital($id){
+		$this->db->where('id', $id);
+		$this->db->update('tb_hospital', array('status' => -1));
+		return $this->db->affected_rows();
+	}
+}//class
+?>
